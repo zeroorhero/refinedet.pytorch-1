633e5433bd0de226bdce4bf7db30790e3fa9bac5
ssssgit <dongdong1114604@126.com>
2018-12-18 20:44:02 +0800

    add resnet101 detection framework

---
+++ unit_test/_init_paths.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+root_dir = os.path.dirname(__file__)
+
+# Add lib to PYTHONPATH
+lib_path = os.path.join(os.path.abspath(
+    os.path.join(root_dir, '../libs')))
+sys.path.append(lib_path)
+
+
+# coco_path = os.path.join(root_dir, 'data', 'coco', 'PythonAPI')
+# add_path(coco_path)
--- unit_test/train.py
+++ unit_test/train.py
@@ -1,52 +1,58 @@
 
-import os
-import sys
-sys.path.append('../')
 
-from data import *
-from utils.augmentations import SSDAugmentation
-from layers.modules.refinedet_loss import BiBoxLoss, MultiBoxLoss
-from refinedet import build_refinedet
 import os
 import sys
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append("..")
+# Add lib to PYTHONPATH
+# lib_path = os.path.join(os.path.abspath(
+#     os.path.join(root_dir, '../libs')))
+# sys.path.append(lib_path)
+
 import time
-import torch
+import argparse
 from torch.autograd import Variable
-import torch.nn as nn
 import torch.optim as optim
 import torch.backends.cudnn as cudnn
-import torch.nn.init as init
 import torch.utils.data as data
-import numpy as np
-import argparse
+from libs.utils.augmentations import SSDAugmentation
+from libs.networks.vgg_refinedet import VGGRefineDet
+from libs.networks.resnet_refinedet import ResNetRefineDet
+from libs.dataset.config import voc, coco, MEANS
+from libs.dataset.coco import COCO_ROOT, COCODetection, COCO_CLASSES
+from libs.dataset.voc0712 import VOC_ROOT, VOCDetection, \
+    VOCAnnotationTransform
+from libs.dataset import *
 import cPickle
-
-from pycallgraph import PyCallGraph, Config, GlobbingFilter
-from pycallgraph.output import GraphvizOutput
-
 
 import pdb
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
 def str2bool(v):
-  return v.lower() in ("yes", "true", "t", "1")
+    return v.lower() in ('yes', 'true', 't', '1')
 
 
 parser = argparse.ArgumentParser(
     description='Single Shot MultiBox Detector Training With Pytorch')
 train_set = parser.add_mutually_exclusive_group()
-parser.add_argument('--dataset', default='COCO', choices=['VOC', 'COCO'],
+parser.add_argument('--dataset', default='VOC', choices=['VOC', 'COCO'],
                     type=str, help='VOC or COCO')
-parser.add_argument('--dataset_root', default='/root/dataset/coco',
+parser.add_argument('--dataset_root', default='/root/dataset/voc/VOCdevkit/',
                     help='Dataset root directory path')
-parser.add_argument('--basenet', default='vgg16_reducedfc.pth',
+# parser.add_argument('--basenet', default='vgg16_reducedfc.pth',
+#                     help='Pretrained base model')
+parser.add_argument('--basenet', default='resnet101.pth',
                     help='Pretrained base model')
-parser.add_argument('--batch_size', default=8, type=int,
+parser.add_argument('--batch_size', default=32, type=int,
                     help='Batch size for training')
 parser.add_argument('--resume', default=None, type=str,
                     help='Checkpoint state_dict file to resume training from')
 parser.add_argument('--start_iter', default=0, type=int,
                     help='Resume training at this iter')
-parser.add_argument('--num_workers', default=4, type=int,
+parser.add_argument('--num_workers', default=8, type=int,
                     help='Number of workers used in dataloading')
 parser.add_argument('--cuda', default=True, type=str2bool,
                     help='Use CUDA to train model')
@@ -60,77 +66,36 @@ parser.add_argument('--gamma', default=0.1, type=float,
                     help='Gamma update for SGD')
 parser.add_argument('--visdom', default=False, type=str2bool,
                     help='Use visdom for loss visualization')
-parser.add_argument('--save_folder', default='weights/',
+parser.add_argument('--save_folder', default='../weights/resnet101',
                     help='Directory for saving checkpoint models')
 args = parser.parse_args()
 
 if torch.cuda.is_available():
-  print('CUDA devices: ', torch.cuda.device)
-  print('GPU numbers: ', torch.cuda.device_count())
+    print('CUDA devices: ', torch.cuda.device)
+    print('GPU numbers: ', torch.cuda.device_count())
 
 if torch.cuda.is_available():
-  if args.cuda:
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-  if not args.cuda:
-    print("WARNING: It looks like you have a CUDA device, but aren't " +
-          "using CUDA.\nRun with --cuda for optimal training speed.")
-    torch.set_default_tensor_type('torch.FloatTensor')
+    if args.cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    if not args.cuda:
+        print('WARNING: It looks like you have a CUDA device, but are not' +
+              'using CUDA.\nRun with --cuda for optimal training speed.')
+        torch.set_default_tensor_type('torch.FloatTensor')
 else:
-  torch.set_default_tensor_type('torch.FloatTensor')
+    torch.set_default_tensor_type('torch.FloatTensor')
 
-
-
-
-def xavier(param):
-  init.xavier_uniform(param)
-
-
-def weights_init(m):
-  if isinstance(m, nn.Conv2d):
-    xavier(m.weight.data)
-    m.bias.data.zero_()
+if not os.path.exists(args.save_folder):
+    os.mkdir(args.save_folder)
 
 def main():
-
-  
-  # run(
-  #   'include',
-  #   trace_filter=trace_filter,
-  #   comment='Should show secret_function.'
-  # )
-  
-  graphviz = GraphvizOutput()
-  graphviz.output_file = 'basic.svg'
-  graphviz.output_type = 'svg'
-  config = Config()
-  trace_filter = GlobbingFilter(include=[
-    'layers.*',
-    'utils.*',
-    'data.*',
-    'refinedet.*'
-  ])
-  # trace_filter = GlobbingFilter(exclude=[
-  #   'pdb.*',
-  #   'pycallgraph.*'
-  # ])
-  
-  config.trace_filter = trace_filter
-  with PyCallGraph(output=graphviz, config=config):
-    # pdb.set_trace()
-    
     images_path = './images.pkl'
     targets_path = './targets.pkl'
     
-    
     args.dataset_root = COCO_ROOT
     cfg = coco
-    
     # data
     # dataset = COCODetection(root=args.dataset_root,
-    #                        transform=SSDAugmentation(cfg['min_dim'],
-    #                                                  MEANS))
-    # step_index = 0
-    #
+    #                         transform=SSDAugmentation(cfg['min_dim'], MEANS))
     # data_loader = data.DataLoader(dataset, args.batch_size,
     #                               num_workers=args.num_workers,
     #                               shuffle=False, collate_fn=detection_collate,
@@ -143,8 +108,12 @@ def main():
     # images_array_list = [Variable(cur_image).data.numpy() for cur_image in images_list]
     # targets_array_list = [Variable(cur_targets).data.numpy() for cur_targets in targets_list]
     # # store data
-    # cPickle.dump(images_array_list, open(images_path, 'wb'))
-    # cPickle.dump(targets_array_list, open(targets_path, 'wb'))
+    # fw_images = open(images_path, 'wb')
+    # cPickle.dump(images_array_list, fw_images)
+    # fw_images.close()
+    # fw_targets = open(targets_path, 'wb')
+    # cPickle.dump(targets_array_list, fw_targets)
+    # fw_targets.close()
     
     # load data
     images_array_list = cPickle.load(open(images_path, 'rb'))
@@ -157,86 +126,45 @@ def main():
     print(type(images))
     print(type(targets))
     
-    
-    # pdb.set_trace()
-    refinedet_net = build_refinedet('train', cfg, cfg['min_dim'],
-                                    cfg['num_classes'])
-    
-    if args.cuda:
-      net = refinedet_net.cuda()
-      # net = torch.nn.DataParallel(refinedet_net).cuda()
-    else:
-      net = refinedet_net
-      
-    device_ids = [0]
-    cudnn.benchmark = True
-    weights_path = '../weights/vgg16_reducedfc.pth'
-    vgg_weights = torch.load(weights_path)
-    refinedet_net.vgg.load_state_dict(vgg_weights)
-    
-    
-    print('Initializing weights...')
-    # initialize newly added layers' weights with xavier method
-    net.extras.apply(weights_init)
-    net.bi_loc.apply(weights_init)
-    net.bi_conf.apply(weights_init)
+    refinedet = ResNetRefineDet(cfg['num_classes'], cfg)
 
-    net.back_pyramid.apply(weights_init)
-    net.multi_loc.apply(weights_init)
-    net.multi_conf.apply(weights_init)
-    
-    optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                          momentum=args.momentum,
+    refinedet.create_architecture(
+        os.path.join(args.save_folder, args.basenet), pretrained=True,
+        fine_tuning=True)
+    net = refinedet
+    if args.cuda:
+        net = torch.nn.DataParallel(refinedet).cuda()
+        cudnn.benchmark = True
+
+    params = net.state_dict()
+    # for k, v in params.items():
+    #     print(k)
+    #     print(v.shape)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum,
                           weight_decay=args.weight_decay)
-    # neg_pos ratio 3:1
-    bi_criterion = BiBoxLoss(0.5, True, 0, True, 3, 0.5,
-                             args.cuda)
-    
-    multi_criterion = MultiBoxLoss(cfg['num_classes'], 0.5, True, 0, True,
-                                   3, 0.5, 0.6, args.cuda)
-    
     net.train()
-    # loss counters
-    print('Loading the dataset...')
     print('Using the specified args:')
     print(args)
-    
-      
+
     if args.cuda:
         images = Variable(images.cuda())
         targets = [Variable(ann.cuda(), volatile=True) for ann in targets]
     else:
         images = Variable(images)
         targets = [Variable(ann, volatile=True) for ann in targets]
-        
+
     # forward
     t0 = time.time()
-    bi_out, multi_out, priors = net(images)
-    # backprop
     optimizer.zero_grad()
-    # pdb.set_trace()
-    bi_loss_l, bi_loss_c = bi_criterion(bi_out, priors, targets)
-    multi_loss_l, multi_loss_c = multi_criterion(bi_out, multi_out, priors, targets)
-    loss = bi_loss_l + bi_loss_c + multi_loss_l + multi_loss_c
-    print(loss.data[0])
-    
+    bi_loss_loc, bi_loss_conf, multi_loss_loc, multi_loss_conf = \
+        net(images, targets)
+    loss = bi_loss_loc + bi_loss_conf + multi_loss_loc + multi_loss_conf
     loss.backward()
     optimizer.step()
     t1 = time.time()
-    
-    bi_loc_loss = 0
-    bi_conf_loss = 0
-    multi_loc_loss = 0
-    multi_conf_loss = 0
-    
-    bi_loc_loss += bi_loss_l.data[0]
-    bi_conf_loss += bi_loss_c.data[0]
-    multi_loc_loss += multi_loss_l.data[0]
-    multi_conf_loss += multi_loss_c.data[0]
-    
     print('timer: %.4f sec.' % (t1 - t0))
 
 
 if __name__ == '__main__':
-  main()
+    main()
 
